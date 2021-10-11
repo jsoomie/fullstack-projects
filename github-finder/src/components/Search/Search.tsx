5b3e544a6178aa8e573660d530e47ba08cba7a51
Jonathan Soukaseume <sksmejn@gmail.com>
2021-10-11 10:28:54 -0700

    feat: add onchange event to search bar

--- github-finder/src/components/Search/Search.tsx
+++ github-finder/src/components/Search/Search.tsx
@@ -1,11 +1,25 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import "./Search.css";
 
 export class Search extends Component {
+  state = {
+    text: "",
+  };
+
+  onChange(e: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({ text: e.currentTarget.value });
+  }
+
   render() {
     return (
       <form action="" id="Search-Form">
-        <input type="text" name="text" placeholder="Search Users..." />
+        <input
+          type="text"
+          name="text"
+          placeholder="Search Users..."
+          onChange={(e) => this.onChange(e)}
+          value={this.state.text}
+        />
         <input type="submit" value="Search" className="button" />
       </form>
     );
