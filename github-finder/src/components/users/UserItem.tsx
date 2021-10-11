1f0d439ff5c1e652afca972b114379210eb76970
Jonathan Soukaseume <sksmejn@gmail.com>
2021-10-10 19:55:53 -0700

    feat: create card and circle avatar image

--- github-finder/src/components/users/UserItem.tsx
+++ github-finder/src/components/users/UserItem.tsx
@@ -1,10 +1,24 @@
 import { Component } from "react";
+import "./UserItem.css";
 
 export class UserItem extends Component {
+  state = {
+    id: "id",
+    login: "mojombo",
+    avatarUrl: "https://avatars0.githubusercontent.com/u/1?v=4",
+    htmlUrl: "https://github.com/mojombo",
+  };
+
   render() {
+    const { login, avatarUrl, htmlUrl } = this.state;
+
     return (
-      <div>
-        <h1>UserItem</h1>
+      <div id="UserItem" className="card">
+        <img src={avatarUrl} alt={login} className="avatar" />
+        <h3>{login}</h3>
+        <a href={htmlUrl} className="button">
+          More
+        </a>
       </div>
     );
   }
