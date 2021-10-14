1e67d0f6e6220bd0191074c4a886928136fca212
Jonathan Soukaseume <sksmejn@gmail.com>
2021-10-14 08:09:30 -0700

    refactor: change app to be a functional comp

--- github-finder/src/App.tsx
+++ github-finder/src/App.tsx
@@ -1,21 +1,18 @@
-import { Component } from "react";
 import { Home, About } from "./pages";
-import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/about" component={About} />
-          </Switch>
-        </Router>
-      </div>
-    );
-  }
-}
+export const App = (): JSX.Element => {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+        </Switch>
+      </Router>
+    </div>
+  );
+};
 
 export default App;
