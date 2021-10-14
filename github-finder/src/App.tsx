d2e5079a3ffc0eb223ee7611dcd1488647a18f51
Jonathan Soukaseume <sksmejn@gmail.com>
2021-10-14 08:05:53 -0700

    feat: create about page skeleton and install react-router-dom and defs

--- github-finder/src/App.tsx
+++ github-finder/src/App.tsx
@@ -1,12 +1,18 @@
 import { Component } from "react";
-import { Home } from "./pages";
+import { Home, About } from "./pages";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Home />
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+          </Switch>
+        </Router>
       </div>
     );
   }
---
+++ github-finder/src/pages/About.tsx
@@ -0,0 +1,9 @@
+export const About = (): JSX.Element => {
+  return (
+    <div>
+      <h1>About this app!</h1>
+      <p>App to search Github Users</p>
+      <p>Version: 1</p>
+    </div>
+  );
+};
