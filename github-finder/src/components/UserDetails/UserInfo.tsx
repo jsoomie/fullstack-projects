ffe3b3a2a261376b26cd6d019e9ad3d417e11a13
Jonathan Soukaseume <sksmejn@gmail.com>
2021-10-14 20:11:19 -0700

    chore: add full info in details and add button to view profile

--- github-finder/src/components/UserDetails/UserInfo.tsx
+++ github-finder/src/components/UserDetails/UserInfo.tsx
@@ -11,6 +11,21 @@ interface UserInfoProps {
 }
 
 export const UserInfo = ({ loading, data }: UserInfoProps) => {
+  const {
+    login,
+    name,
+    avatar_url,
+    html_url,
+    hireable,
+    bio,
+    blog,
+    followers,
+    following,
+    location,
+    public_gists,
+    public_repos,
+  } = data;
+
   return (
     <Fragment>
       {!loading && data ? (
@@ -20,32 +35,37 @@ export const UserInfo = ({ loading, data }: UserInfoProps) => {
             Back to search
           </Link>
           <div className="card">
-            {data.name ? (
+            {name ? (
               <h2>
-                {data.name} ({data.login})
+                {name} ({login})
               </h2>
             ) : (
-              <h2>{data.login}</h2>
+              <h2>{login}</h2>
             )}
             <img
-              src={data.avatar_url}
-              alt={data.login}
+              src={avatar_url}
+              alt={login}
               className="avatar"
               style={{ height: "10rem", width: "10rem" }}
             />
             <p>
               Hireable:{" "}
-              {data.hireable ? (
+              {hireable ? (
                 <FaCheck style={{ color: "green" }} />
               ) : (
                 <FaTimesCircle style={{ color: "red" }} />
               )}{" "}
             </p>
-            {data.bio && <p>{data.bio}</p>}
-            <p>Blog: {data.blog}</p>
-            <p>Followers: {data.followers}</p>
-            <p>Following: {data.following}</p>
-            <p>{data.location}</p>
+            {bio ? <p>Bio: {bio}</p> : null}
+            {blog ? <p>Blog: {blog}</p> : null}
+            {followers ? <p>Followers: {followers}</p> : null}
+            {following ? <p>Following: {following}</p> : null}
+            {location ? <p>Location: {location}</p> : null}
+            {public_gists ? <p>Public Gists: {public_gists}</p> : null}
+            {public_repos ? <p>Public Repos: {public_repos}</p> : null}
+            <a href={html_url} className="button" style={{ color: "#fff" }}>
+              View Profile
+            </a>
           </div>
         </div>
       ) : (
