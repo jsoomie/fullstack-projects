7a7835eb456dc117dbb35dddae0d3f2e4a868c56
Jonathan Soukaseume <sksmejn@gmail.com>
2021-10-25 09:15:46 -0700

    feat: create update contacts

--- contact-keeper/src/controllers/contacts.ts
+++ contact-keeper/src/controllers/contacts.ts
@@ -61,9 +61,33 @@ export const postContacts: Controllers = async (req, res) => {
  * @description   Updates contacts
  * @route         PUT /api/contacts/:id
  */
-export const updateContacts: Controllers = (req, res) => {
+export const updateContacts: Controllers = async (req, res) => {
+  const { name, email, phone, type } = req.body;
+  const contactFields: {
+    name?: string;
+    email?: string;
+    phone?: number;
+    type?: string;
+  } = {};
+
+  if (name) contactFields.name = name;
+  if (email) contactFields.email = email;
+  if (phone) contactFields.phone = phone;
+  if (type) contactFields.type = type;
+
   try {
-    res.json({ msg: "[CONTROLLER] PUT api/contacts/" });
+    let contact = await Contact.findById(req.params.id);
+    if (!contact) return res.status(404).json({ msg: "Contact not found" });
+    if (req.user)
+      if (contact.user.toString() !== req.user.id) {
+        return res.status(401).json({ msg: "Not authorized" });
+      }
+    contact = await Contact.findByIdAndUpdate(
+      req.params.id,
+      { $set: contactFields },
+      { new: true }
+    );
+    res.json(contact);
   } catch (err) {
     console.error(err);
     res.json(err).status(500);
