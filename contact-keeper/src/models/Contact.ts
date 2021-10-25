b196601de6575d33198c0566aa908f4e3c2cc961
Jonathan Soukaseume <sksmejn@gmail.com>
2021-10-25 07:33:06 -0700

    chore: create basic contact model

---
+++ contact-keeper/src/models/Contact.ts
@@ -0,0 +1,29 @@
+import { Schema, model } from "mongoose";
+
+const ContactSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users",
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+  },
+  type: {
+    type: String,
+    default: "personal",
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export const Contact = model("Contact", ContactSchema);
