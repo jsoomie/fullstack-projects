012f47adbb3abd60926eed07dad5c98990854a51
Jonathan Soukaseume <sksmejn@gmail.com>
2021-10-24 21:20:04 -0700

    feat: create middleware to check auth

---
+++ contact-keeper/src/middlewares/auth.ts
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken";
+import { Response, Request, NextFunction } from "express";
+
+interface IUserRequest extends Request {
+  user: any;
+}
+
+type tokenChecker = (
+  req: IUserRequest,
+  res: Response,
+  next: NextFunction
+) => void;
+export const tokenChecker: tokenChecker = (req, res, next) => {
+  const token = <string>req.header("x-auth-token");
+
+  if (!token) {
+    return res.status(401).json({ msg: "No token, authorization denied" });
+  }
+
+  const hushies = process.env.JWT_HUSH;
+
+  try {
+    if (token && hushies) {
+      const decoded = <any>jwt.verify(token, hushies);
+      req.user = decoded.user;
+      next();
+    }
+  } catch (error) {
+    res.status(401).json({ msg: "Token is not valid" });
+  }
+};
