b0c50345ea7091cc5358df3030d2b033014e6df5
Jonathan Soukaseume <sksmejn@gmail.com>
2021-10-27 14:51:17 -0700

    chore: update about page with a container

--- contact-keeper/client/src/pages/About.tsx
+++ contact-keeper/client/src/pages/About.tsx
@@ -1,12 +1,17 @@
+import { Navbar } from "../components";
+
 export const About = (): JSX.Element => {
   return (
     <div>
-      <h1>Contact Keeper</h1>
-      <h3>Welcome to the Contact Keeper!</h3>
-      <p>
-        Where you can create your own personal contacts books with name, phone
-        numbers, emails and addresses!
-      </p>
+      <Navbar />
+      <div className="container">
+        <h1>Contact Keeper</h1>
+        <h3>Welcome to the Contact Keeper!</h3>
+        <p>
+          Where you can create your own personal contacts books with name, phone
+          numbers, emails and addresses!
+        </p>
+      </div>
     </div>
   );
 };
