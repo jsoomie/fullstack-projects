cfde0b09f6e673d645eb814137033bd9cef63783
Jonathan Soukaseume <sksmejn@gmail.com>
2021-10-29 10:36:15 -0700

    chore: prep for contact component

---
+++ contact-keeper/client/src/components/Contacts/ContactItem.tsx
@@ -0,0 +1,7 @@
+export const ContactItems = (): JSX.Element => {
+  return (
+    <div>
+      <h1>ContactItems</h1>
+    </div>
+  );
+};
---
+++ contact-keeper/client/src/components/Contacts/Contacts.tsx
@@ -0,0 +1,8 @@
+import { ContactItems } from "./ContactItem";
+export const Contacts = (): JSX.Element => {
+  return (
+    <div>
+      <ContactItems />
+    </div>
+  );
+};
