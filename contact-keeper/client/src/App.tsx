9c882566320dc04441d2f373c59ce04683d13286
Jonathan Soukaseume <sksmejn@gmail.com>
2021-10-29 10:21:10 -0700

    chore: wrapped app.tsx with contact context

--- contact-keeper/client/src/App.tsx
+++ contact-keeper/client/src/App.tsx
@@ -1,16 +1,19 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Home, About } from "./pages";
+import { ContactState } from "./context";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
-        </Switch>
-      </Router>
+      <ContactState>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+          </Switch>
+        </Router>
+      </ContactState>
     </div>
   );
 }
--- contact-keeper/client/src/context/index.ts
+++ contact-keeper/client/src/context/index.ts
@@ -1,1 +1,15 @@
-export {};
+export {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER,
+  SET_ALERT,
+  REMOVE_ALERT,
+} from "./actions";
+
+export { contactContext } from "./contact/contactContext";
+export { contactReducer } from "./contact/contactReducer";
+export { ContactState } from "./contact/contactState";
