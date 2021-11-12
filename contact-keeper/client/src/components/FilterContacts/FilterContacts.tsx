f1f08882309727ad0ed04b65b9bad9360b595583
Jonathan Soukaseume <sksmejn@gmail.com>
2021-11-12 08:04:40 -0800

    chore: add useRef to filterContact comp

--- contact-keeper/client/src/components/FilterContacts/FilterContacts.tsx
+++ contact-keeper/client/src/components/FilterContacts/FilterContacts.tsx
@@ -1,12 +1,21 @@
-import { useContext } from "react";
+import { useContext, useRef, ChangeEvent } from "react";
 import { contactContext } from "../../context";
 
 export const ContactFilter = () => {
-  const context = useContext(contactContext);
+  const { filteredContacts, clearFiltered } = useContext(contactContext);
+  const text = useRef("");
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (text.current !== "") {
+      filteredContacts(e.currentTarget.value);
+    } else {
+      clearFiltered();
+    }
+  };
 
   return (
-    <div>
-      <h1>Filter Here</h1>
-    </div>
+    <form>
+      <input type="text" placeholder="Filter Contacts..." onChange={onChange} />
+    </form>
   );
 };
