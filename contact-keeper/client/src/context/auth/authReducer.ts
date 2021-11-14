7602bd3d068abaf598251eb030795e8e22b6a70f
Jonathan Soukaseume <sksmejn@gmail.com>
2021-11-13 18:29:13 -0800

    chore: create skeleton reducer with cases and actions

--- contact-keeper/client/src/context/auth/authReducer.ts
+++ contact-keeper/client/src/context/auth/authReducer.ts
@@ -10,6 +10,34 @@ import {
 
 export const authReducer = (state: any, action: any) => {
   switch (action.type) {
+    case REGISTER_SUCCESS:
+      return {
+        ...state,
+      };
+    case REGISTER_FAIL:
+      return {
+        ...state,
+      };
+    case USER_LOADED:
+      return {
+        ...state,
+      };
+    case AUTH_ERROR:
+      return {
+        ...state,
+      };
+    case LOGIN_FAIL:
+      return {
+        ...state,
+      };
+    case LOGOUT:
+      return {
+        ...state,
+      };
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+      };
     default:
       return state;
   }
