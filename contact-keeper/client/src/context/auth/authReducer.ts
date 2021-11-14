e317e70b5c3561328a37fb500de20fe659c9e1e4
Jonathan Soukaseume <sksmejn@gmail.com>
2021-11-13 18:47:23 -0800

    chore: create type for actions of authreducer

--- contact-keeper/client/src/context/auth/authReducer.ts
+++ contact-keeper/client/src/context/auth/authReducer.ts
@@ -8,7 +8,17 @@ import {
   CLEAR_ERRORS,
 } from "../actions";
 
-export const authReducer = (state: any, action: any) => {
+import { IAuth } from "./authContext";
+
+type Action =
+  | { type: typeof REGISTER_SUCCESS }
+  | { type: typeof REGISTER_FAIL }
+  | { type: typeof USER_LOADED }
+  | { type: typeof AUTH_ERROR }
+  | { type: typeof LOGIN_FAIL }
+  | { type: typeof LOGOUT }
+  | { type: typeof CLEAR_ERRORS };
+export const authReducer = (state: IAuth, action: Action): IAuth => {
   switch (action.type) {
     case REGISTER_SUCCESS:
       return {
