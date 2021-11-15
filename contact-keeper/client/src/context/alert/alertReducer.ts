44453fa6a32b252fc09be109d8078b4acd847c9b
Jonathan Soukaseume <sksmejn@gmail.com>
2021-11-15 11:31:09 -0800

    chore: crate alert reducer set and remove with any types for now

--- contact-keeper/client/src/context/alert/alertReducer.ts
+++ contact-keeper/client/src/context/alert/alertReducer.ts
@@ -1,5 +1,11 @@
+import { SET_ALERT, REMOVE_ALERT } from "../actions";
+
 export const alertReducer = (state: any, action: any) => {
   switch (action.type) {
+    case SET_ALERT:
+      return [...state, action.payload];
+    case REMOVE_ALERT:
+      return state.filter((alert: any) => alert.id !== action.payload);
     default:
       return state;
   }
