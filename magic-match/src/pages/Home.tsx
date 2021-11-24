446c7024f7e5d487064cec63aedc0312dcfad5cc
Jonathan Soukaseume <sksmejn@gmail.com>
2021-11-24 07:57:13 -0800

    feat: create card shuffle

--- magic-match/src/pages/Home.tsx
+++ magic-match/src/pages/Home.tsx
@@ -1,10 +1,43 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
+
+interface Cards {
+  src: string;
+}
+
+const imageName = [
+  "helmet-1",
+  "potion-1",
+  "ring-1",
+  "scroll-1",
+  "shield-1",
+  "sword-1",
+];
+
+const cardImages: Cards[] = [];
+for (let i = 0; i < imageName.length; i++) {
+  cardImages.push({
+    src: `/img/${imageName[i]}.png`,
+  });
+}
 
 export const Home = () => {
+  const [cards, setCards] = useState<Cards[] | null>(null);
+  const [turns, setTurns] = useState<number>(0);
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  console.log(cards, turns);
+
   return (
     <Fragment>
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
     </Fragment>
   );
 };
