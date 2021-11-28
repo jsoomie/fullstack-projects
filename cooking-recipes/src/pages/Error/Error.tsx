9e98211fa0aef2b5aa96279c2683ae2e5d741c5e
Jonathan Soukaseume <sksmejn@gmail.com>
2021-11-28 12:36:48 -0800

    feat: create quick error 404 page

--- cooking-recipes/src/pages/Error/Error.tsx
+++ cooking-recipes/src/pages/Error/Error.tsx
@@ -1,9 +1,14 @@
+import confusedImage from "./img/confused.jpg";
 import "./Error.css";
 
 export const Error = () => {
   return (
-    <div>
-      <h1>Error</h1>
+    <div id="ErrorPage">
+      <img src={confusedImage} alt="confused" />
+
+      <h1>404 - Missing Page</h1>
+      <p>The page you are looking for is either missing or doesn't exist</p>
+      <a href="/">Click here to return to homepage</a>
     </div>
   );
 };
