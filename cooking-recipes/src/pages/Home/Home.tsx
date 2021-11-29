e6e2e5fb8a912ff7c6e4db891d16572f6abbdee2
Jonathan Soukaseume <sksmejn@gmail.com>
2021-11-29 09:08:45 -0800

    feat: fetch data and create type for data within interfaces create interface of IRecipe

--- cooking-recipes/src/pages/Home/Home.tsx
+++ cooking-recipes/src/pages/Home/Home.tsx
@@ -1,9 +1,18 @@
+import { useFetch } from "hooks";
+import { IRecipe } from "interfaces";
 import "./Home.css";
 
 export const Home = () => {
+  const localURL = "http://localhost:3000/recipes";
+
+  const { data, isPending, error } = useFetch<IRecipe[]>(localURL);
+
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="home">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {data &&
+        data.map((recipe: IRecipe) => <h2 key={recipe.id}>{recipe.title}</h2>)}
     </div>
   );
 };
