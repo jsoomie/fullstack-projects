0e7a22003da5a4430c2992d7a82be44e56706b29
Jonathan Soukaseume <sksmejn@gmail.com>
2021-11-29 16:26:14 -0800

    chore: add single recipe page

--- cooking-recipes/src/pages/Recipe/Recipe.tsx
+++ cooking-recipes/src/pages/Recipe/Recipe.tsx
@@ -1,10 +1,18 @@
+import { useParams } from "react-router-dom";
 import { useFetch } from "hooks";
+import { IRecipe } from "interfaces";
 import "./Recipe.css";
 
 export const Recipe = () => {
+  const { id } = useParams();
+  const localURL = `http://localhost:8000/recipes/${id}`;
+  const { data, isPending, error } = useFetch<IRecipe>(localURL);
+
   return (
-    <div>
-      <h1>Recipe Page</h1>
+    <div className="recipe">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {data && <h1>{data.title}</h1>}
     </div>
   );
 };
