2f2ecd3a34f8362d0341ee1728c3677b2522079a
Jonathan Soukaseume <sksmejn@gmail.com>
2021-12-03 09:45:00 -0800

    chore: create login page

--- money-tracker/src/pages/Login/Login.tsx
+++ money-tracker/src/pages/Login/Login.tsx
@@ -1,9 +1,35 @@
-import style from "./Login.module.css";
+import { FormEvent, useState } from "react";
+import styles from "./Login.module.css";
 
 export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log({ email, password });
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <form className={styles["login-form"]} onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <label>
+        <span>email: </span>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password: </span>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <button className="btn">Login</button>
+    </form>
   );
 };
