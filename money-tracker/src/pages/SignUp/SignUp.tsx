e225ebe395ceffdb652fb985a7619fcab636fe71
Jonathan Soukaseume <sksmejn@gmail.com>
2021-12-03 09:51:04 -0800

    chore: create sign up page

--- money-tracker/src/pages/SignUp/SignUp.tsx
+++ money-tracker/src/pages/SignUp/SignUp.tsx
@@ -1,9 +1,44 @@
-import style from "./SignUp.module.css";
+import { FormEvent, useState } from "react";
+import styles from "./SignUp.module.css";
 
 export const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log({ email, password, displayName });
+  };
+
   return (
-    <div>
-      <h1>SignUp</h1>
-    </div>
+    <form className={styles["signup-form"]} onSubmit={handleSubmit}>
+      <h2>Sign Up</h2>
+      <label>
+        <span>name: </span>
+        <input
+          type="text"
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+      <label>
+        <span>email: </span>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password: </span>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <button className="btn">Register</button>
+    </form>
   );
 };
