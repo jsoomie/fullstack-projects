ea7496a23c44918554af4395e697850f07775bd0
Jonathan Soukaseume <sksmejn@gmail.com>
2021-12-02 16:34:29 -0800

    refactor: use search hack of searching through titles

--- cooking-recipes/src/pages/Search/Search.tsx
+++ cooking-recipes/src/pages/Search/Search.tsx
@@ -1,14 +1,48 @@
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { IRecipe } from "interfaces";
-import { useFetch } from "hooks";
 import { RecipeList } from "components";
-import "./Search.css";
+import { DB, projectFirestore } from "firebase";
 
 export const Search = () => {
   const queryString = useLocation().search;
   const query = new URLSearchParams(queryString).get("q");
-  const localURL = `http://localhost:8000/recipes?q=${query}`;
-  const { error, isPending, data } = useFetch<IRecipe[]>(localURL);
+  const [data, setData] = useState<IRecipe[] | null>(null);
+  const [isPending, setIsPending] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    setIsPending(true);
+
+    const unsub = projectFirestore.collection(DB.RECIPES).onSnapshot(
+      (res) => {
+        if (res.empty) {
+          setError("No recipe to load...");
+          setIsPending(false);
+        } else {
+          let results: IRecipe[] = [];
+          res.docs.forEach((doc) => {
+            results.push({ id: doc.id, ...doc.data() } as IRecipe);
+          });
+          if (query) {
+            setData(
+              results.filter((doc) => doc.title.toLowerCase().includes(query))
+            );
+            setIsPending(false);
+          } else {
+            setError("No query search found...");
+            setIsPending(false);
+          }
+        }
+      },
+      (error) => {
+        setError(error.message);
+        setIsPending(false);
+      }
+    );
+
+    return () => unsub();
+  }, [query]);
 
   return (
     <div>
