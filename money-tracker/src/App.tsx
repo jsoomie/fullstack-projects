5d94674c22de122b8b78384bcd65001b6fb1ba0a
Jonathan Soukaseume <sksmejn@gmail.com>
2021-12-03 19:35:57 -0800

    chore: create conditional rendering of pages also map through routes instead

--- money-tracker/src/App.tsx
+++ money-tracker/src/App.tsx
@@ -1,16 +1,37 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { Home, Login, SignUp } from "pages";
 import { Navbar } from "components";
+import { useAuthContext } from "hooks";
+import React from "react";
 
 function App() {
+  const { authIsReady, user } = useAuthContext();
+  const routes = [
+    {
+      path: "/",
+      element: user ? <Home /> : <Navigate to="/login" />,
+    },
+    {
+      path: "/login",
+      element: !user ? <Login /> : <Navigate to="/" />,
+    },
+    {
+      path: "/signup",
+      element: !user ? <SignUp /> : <Navigate to="/" />,
+    },
+  ];
   return (
     <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
+      {authIsReady && (
+        <React.Fragment>
+          <Navbar />
+          <Routes>
+            {routes.map(({ path, element }) => (
+              <Route path={path} element={element} key={path} />
+            ))}
+          </Routes>
+        </React.Fragment>
+      )}
     </div>
   );
 }
