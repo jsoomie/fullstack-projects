9303636c62129d45a4de8dda6199b451dd90d23f
Jonathan Soukaseume <sksmejn@gmail.com>
2021-12-03 18:34:08 -0800

    chore: create conditionally rendering of login and logout button

--- money-tracker/src/components/Navbar/Navbar.tsx
+++ money-tracker/src/components/Navbar/Navbar.tsx
@@ -1,27 +1,38 @@
 import { Link } from "react-router-dom";
 import { useLogout } from "hooks";
+import { useAuthContext } from "hooks";
 import styles from "./Navbar.module.css";
-
-const { navbar, title } = styles;
 
 export const Navbar = () => {
   const { logout } = useLogout();
+  const { user } = useAuthContext();
 
   return (
-    <nav className={navbar}>
+    <nav className={styles.navbar}>
       <ul>
-        <li className={title}>money-tracker</li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/signup">Signup</Link>
-        </li>
-        <li>
-          <button className="btn" onClick={logout}>
-            Logout
-          </button>
-        </li>
+        <li className={styles.title}>money-tracker</li>
+
+        {!user && (
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </>
+        )}
+
+        {user && (
+          <>
+            <li>Hello, {user.displayName}</li>
+            <li>
+              <button className="btn" onClick={logout}>
+                Logout
+              </button>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
