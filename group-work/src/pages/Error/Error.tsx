81db57945f4af7b46c3b1b50b1974efae9154aae
Jonathan Soukaseume <sksmejn@gmail.com>
2021-12-14 18:49:37 -0800

    chore: create basic skeleton of error page

---
+++ group-work/src/pages/Error/Error.tsx
@@ -0,0 +1,11 @@
+import "./Error.css";
+
+export const Error = () => {
+  return (
+    <div>
+      <h1>404 - Page is missing</h1>
+      <p>The page you are looking for is missing or doesn't exist.</p>
+      <a href="/">Please click here to return to the homepage</a>
+    </div>
+  );
+};
