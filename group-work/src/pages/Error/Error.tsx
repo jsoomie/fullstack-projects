927ada9e293cf44e0be31ef3ef01e3cf76cf3c8b
Jonathan Soukaseume <sksmejn@gmail.com>
2021-12-16 07:49:38 -0800

    style: error page with ?icon

--- group-work/src/pages/Error/Error.tsx
+++ group-work/src/pages/Error/Error.tsx
@@ -3,7 +3,11 @@ import "./Error.css";
 export const Error = () => {
   return (
     <div className="errordiv">
-      <h1>404 - Page is missing</h1>
+      <h1>
+        <span className="firstQuestion">?</span>?
+        <span className="secondQuestion">?</span>
+      </h1>
+      <h2>404 - Page is missing</h2>
       <p>The page you are looking for is missing or doesn't exist.</p>
       <a href="/">Please click here to return to the homepage</a>
     </div>
