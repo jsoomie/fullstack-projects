d4ed46ae8aafda520a6de5806183c674e7f34115
Jonathan Soukaseume <sksmejn@gmail.com>
2021-12-16 07:57:34 -0800

    chore: create basic outline of sidebar

---
+++ group-work/src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,30 @@
+import { DashboardIcon, AddIcon } from "assets";
+import { NavLink } from "react-router-dom";
+import "Sidebar.css";
+
+export const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sidebar-content">
+        <div className="user">{/* Avatar and user name here */}</div>
+        <p>Hey user</p>
+      </div>
+      <nav className="links">
+        <ul>
+          <li>
+            <NavLink to="/">
+              <img src={DashboardIcon} alt="dashboard" />
+              <span>Dashboard</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/create">
+              <img src={AddIcon} alt="add project icon" />
+              <span>New Project</span>
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
