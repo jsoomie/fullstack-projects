915d8a20f746805d6aa7c5d85eebea0fec63f6bd
Jonathan Soukaseume <sksmejn@gmail.com>
2021-12-18 08:15:04 -0800

    chore: create signup page and placeholder for file thumbnail upload

--- group-work/src/pages/Signup/Signup.tsx
+++ group-work/src/pages/Signup/Signup.tsx
@@ -1,9 +1,47 @@
+import { useState } from "react";
 import "./Signup.css";
 
 export const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [thumbnail, setThumbnail] = useState<string | null>(null);
+
   return (
-    <div>
-      <h1>Hello Signup</h1>
-    </div>
+    <form className="auth-form">
+      <h2>Sign Up</h2>
+      <label>
+        <span>email: </span>
+        <input
+          type="email"
+          required
+          onChange={(e) => setEmail(e.currentTarget.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password: </span>
+        <input
+          type="password"
+          required
+          onChange={(e) => setPassword(e.currentTarget.value)}
+          value={password}
+        />
+      </label>
+      <label>
+        <span>displayName: </span>
+        <input
+          type="displayName"
+          required
+          onChange={(e) => setDisplayName(e.currentTarget.value)}
+          value={displayName}
+        />
+      </label>
+      <label>
+        <span>profile thumbnail: </span>
+        <input type="file" required />
+      </label>
+      <button className="btn">Sign Up</button>
+    </form>
   );
 };
